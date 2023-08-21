7e2798d4b2a96256abbc8ca35f864894fa742e2b
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-08-21 19:26:55 +0200

    inserimento del layout della rotta admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('layouts.admin');
 });
 
 // Route::get('/dashboard', function () {
