2beb8eafb451192776fef4ecbf1e8f22c9db0623
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-08-22 16:21:12 +0200

    inserita la tabella della dashboard

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,6 +2,42 @@
 
 @section('content')
 
-    <h1>ciao</h1>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <div class="card-title">
+                        <h3>My Projects</h3>
+                    </div>
+                </div>
+
+                <div class="card-body p-0">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Titolo</th>
+                                <th>Descrizione progetto</th>
+                                <th>Slug</th>
+                                
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($projects as $project)
+                                <tr>
+                                    <td>{{ $project->title}}</td>
+                                    <td>{{ $project->content}}</td>
+                                    <td>{{ $project->slug}}</td>
+                                    
+                                   
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
     
 @endsection
